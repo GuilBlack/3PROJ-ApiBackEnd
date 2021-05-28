ad4a0dd333de4530d11c99a91d703a94a6e9d919
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2021-05-28 18:50:21 +0400

    changed response of the login

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -85,10 +85,16 @@ const login = (req, res) => {
 	if (req.isAuthenticated()) {
 		const { _id, email, firstName, lastName, role } = req.user;
 		const token = signToken(_id);
-		res.cookie("access-token", token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: true });
-		res.cookie("auth-user", "authenticated!", { maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: true }); // cookie that can be read from the web client
+		res.cookie("access-token", token, {
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+			httpOnly: true,
+			sameSite: true,
+		});
+		res.cookie("auth-user", "authenticated!", {
+			maxAge: 7 * 24 * 60 * 60 * 1000,
+			sameSite: true,
+		}); // cookie that can be read from the web client
 		res.status(200).json({
-			isAuthenticated: true,
 			user: {
 				email: email,
 				firstName: firstName,
