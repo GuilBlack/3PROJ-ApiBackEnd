2e46091c2a445690b9645b735f73a4bbeaded57a
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2021-06-05 22:40:04 +0400

    customer cannot reserve more than once in the same time slot + response for get all reservation changed

--- src/Controllers/reservationController.js
+++ src/Controllers/reservationController.js
@@ -29,71 +29,96 @@ const makeReservation = (req, res) => {
 			}
 
 			if (req.body.timeSlot && req.body.seats && checkEmail) {
-				let freeTables = [];
-
-				tableArrangement.layout.forEach((table) => {
+				let checkIfReserved = false;
+				for (i = 0; i < tableArrangement.layout.length; i++) {
 					if (
-						!table.reservations[req.body.timeSlot - 1].isReserved &&
-						table.hasTable
+						tableArrangement.layout[i].reservations[
+							req.body.timeSlot - 1
+						].customer === email
 					) {
-						freeTables.push(table);
-					}
-				});
-
-				freeTables.sort((a, b) => {
-					return Number(a.capacity) - Number(b.capacity);
-				});
-
-				let reservedTables = recursiveShit(
-					freeTables,
-					req.body.timeSlot,
-					req.body.seats,
-					[]
-				);
-
-				reservedTables.forEach((table) => {
-					table.reservations[req.body.timeSlot - 1].customer = email;
-					table.reservations[req.body.timeSlot - 1].isReserved = true;
-					table.reservations[
-						req.body.timeSlot - 1
-					].totalNumberOfPeople = req.body.seats;
-				});
-
-				tableArrangement.save((err, doc) => {
-					if (err)
-						res.status(500).json({
+						checkIfReserved = true;
+						res.status(400).json({
 							message:
-								"Couldn't make reservation due to a problem with the db.",
+								"You already made a reservation for this time slot.",
 						});
-					else {
-						let reservedTablesForUser = [];
-
-						for (i = 0; i < 4; i++) {
-							let timeSlot = {
-								timeSlot: i + 1,
-								totalNumberOfPeople: null,
-								tables: [],
-							};
-							doc.layout.forEach((table) => {
-								if (table.hasTable === true) {
-									if (
-										table.reservations[i].customer === email
-									) {
-										timeSlot.tables.push(table.position);
-										timeSlot.totalNumberOfPeople =
-											table.reservations[
-												i
-											].totalNumberOfPeople;
-									}
-								}
-							});
-							if (timeSlot.tables.length > 0) {
-								reservedTablesForUser.push(timeSlot);
-							}
-						}
-						res.status(200).json(reservedTablesForUser);
+						break;
 					}
-				});
+				}
+
+				if (!checkIfReserved) {
+					let freeTables = [];
+
+					tableArrangement.layout.forEach((table) => {
+						if (
+							!table.reservations[req.body.timeSlot - 1]
+								.isReserved &&
+							table.hasTable
+						) {
+							freeTables.push(table);
+						}
+					});
+
+					freeTables.sort((a, b) => {
+						return Number(a.capacity) - Number(b.capacity);
+					});
+
+					let reservedTables = recursiveReservation(
+						freeTables,
+						req.body.timeSlot,
+						req.body.seats,
+						[]
+					);
+
+					reservedTables.forEach((table) => {
+						table.reservations[req.body.timeSlot - 1].customer =
+							email;
+						table.reservations[
+							req.body.timeSlot - 1
+						].isReserved = true;
+						table.reservations[
+							req.body.timeSlot - 1
+						].totalNumberOfPeople = req.body.seats;
+					});
+
+					tableArrangement.save((err, doc) => {
+						if (err)
+							res.status(500).json({
+								message:
+									"Couldn't make reservation due to a problem with the db.",
+							});
+						else {
+							let reservedTablesForUser = [];
+
+							for (i = 0; i < 4; i++) {
+								let timeSlot = {
+									timeSlot: i + 1,
+									totalNumberOfPeople: null,
+									tables: [],
+								};
+								doc.layout.forEach((table) => {
+									if (table.hasTable === true) {
+										if (
+											table.reservations[i].customer ===
+											email
+										) {
+											timeSlot.tables.push(
+												table.position
+											);
+											timeSlot.totalNumberOfPeople =
+												table.reservations[
+													i
+												].totalNumberOfPeople;
+										}
+									}
+								});
+								if (timeSlot.tables.length > 0) {
+									reservedTablesForUser.push(timeSlot);
+								}
+							}
+							res.status(200).json(reservedTablesForUser);
+						}
+					});
+				}
 			} else {
 				if (checkEmail) {
 					res.status(400),
@@ -104,7 +129,7 @@ const makeReservation = (req, res) => {
 	});
 };
 
-function recursiveShit(freeTables, timeSlot, seats, reservedTables) {
+function recursiveReservation(freeTables, timeSlot, seats, reservedTables) {
 	if (freeTables.length === 0) {
 		return [];
 	}
@@ -123,7 +148,7 @@ function recursiveShit(freeTables, timeSlot, seats, reservedTables) {
 			reservedTables.push(freeTables[freeTables.length - 1]);
 			const capacity = freeTables[freeTables.length - 1].capacity;
 			freeTables.splice(freeTables.length - 1, 1);
-			return recursiveShit(
+			return recursiveReservation(
 				freeTables,
 				timeSlot,
 				seats - capacity,
@@ -139,7 +164,7 @@ function recursiveShit(freeTables, timeSlot, seats, reservedTables) {
 		reservedTables.push(freeTables[freeTables.length - 1]);
 		const capacity = freeTables[freeTables.length - 1].capacity;
 		freeTables.splice(freeTables.length - 1, 1);
-		return recursiveShit(
+		return recursiveReservation(
 			freeTables,
 			timeSlot,
 			seats - capacity,
@@ -197,11 +222,43 @@ const getAllReservations = (req, res) => {
 				});
 			} else {
 				let reservedTables = [];
-				tableArrangement.layout.forEach((table) => {
-					if (table.hasTable === true) {
-						reservedTables.push(table);
-					}
-				});
+
+				for (i = 0; i < 4; i++) {
+					let timeSlot = {
+						timeSlot: i + 1,
+						reservations: [],
+					};
+					tableArrangement.layout.forEach((table) => {
+						if (table.hasTable === true) {
+							let reservationIndex = null;
+							for (j = 0; j < timeSlot.reservations.length; j++) {
+								if (
+									table.reservations[i].customer ===
+									timeSlot.reservations[j].customer
+								) {
+									reservationIndex = j + 1;
+								}
+							}
+							if (reservationIndex) {
+								timeSlot.reservations[
+									reservationIndex - 1
+								].tables.push(table.position);
+							} else {
+								if (table.reservations[i].isReserved) {
+									timeSlot.reservations.push({
+										customer:
+											table.reservations[i].customer,
+										totalNumberOfPeople:
+											table.reservations[i]
+												.totalNumberOfPeople,
+										tables: [table.position],
+									});
+								}
+							}
+						}
+					});
+					reservedTables.push(timeSlot);
+				}
 				res.status(200).json(reservedTables);
 			}
 		});
